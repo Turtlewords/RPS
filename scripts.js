06055ba5e250c903eda3c94b2b4ea690747875fe
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2023-12-21 19:11:05 -0600

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,29 @@
-function getComputerChoice(x) {
+function getComputerChoice() {
     var options = ["Rock", "Paper", "Scissors"];
     var choice = options[Math.floor(Math.random()*options.length)];
     console.log(choice);
     return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    var playerSelection = prompt("Rock, Paper, or Scissors?");
+    var computerSelection = getComputerChoice();
+    
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "Tie!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        return "You Lose! Paper Beats Rock.";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        return "You Win. Rock Beats Scissors";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        return "You Lose. Scissors Beats Paper";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return "You Win. Paper Beats Rock!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+        return "You Lose! Rock Beats Scissors!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return "You Win! Scissors Beats Paper";
+    }
+      
+
 }
